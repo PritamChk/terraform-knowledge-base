82c61a79e46dc312538d015ba7273faabf0be744
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2025-12-12 02:15:34 +0600

    fix: update output references for VM public IPs in output.tf

--- aws/modulesExamples/output.tf
+++ aws/modulesExamples/output.tf
@@ -1,7 +1,7 @@
 output "app_vm_public_ip" {
-  value = module.appVm-1.public_ip
+  value = module.appVm-1.vm-public-address
 }
 
 output "db_vm_public_ip" {
-  value = module.dbVm-1.public_ip
+  value = module.dbVm-1.vm-public-address
 }
