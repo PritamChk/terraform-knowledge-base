906eb58a1afa479ea6dcba109a34c988aaab65a8
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2025-12-27 18:14:08 +0600

    some more outputs added

--- aws/host_plane_pmapp/outputs.tf
+++ aws/host_plane_pmapp/outputs.tf
@@ -5,3 +5,19 @@ output "vm_type_show" {
 output "pmapp_vm_public_ip" {
   value = aws_instance.pmapp_vm.public_ip
 }
+
+output "vpc_id" {
+  value = aws_vpc.pmapp_vpc.id
+}
+
+output "vpc_cidr_block" {
+  value = aws_vpc.pmapp_vpc.cidr_block
+}
+
+output "sec_group_id" {
+  value = aws_security_group.pm_app_sg.id
+}
+
+output "sec_group_id" {
+  value = aws_security_group.pm_app_sg.id
+}
