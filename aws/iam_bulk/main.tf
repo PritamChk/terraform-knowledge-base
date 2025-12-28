ce04bbe6cbbdf97215e846e0ec8793c2f7aba873
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2025-12-29 02:14:56 +0600

    list to set conversion

--- aws/iam_bulk/main.tf
+++ aws/iam_bulk/main.tf
@@ -13,7 +13,7 @@ resource "aws_iam_group" "groups" {
 
 resource "aws_iam_user" "project_IAM_user" {
 
-  for_each = local.users
+  for_each = { for user in local.users : user => user.email }
   name     = lower("${each.value.first_name}_${substr(each.value.last_name, 0, 4)}")
   path     = "/${lower(each.value.role)}/"
 
