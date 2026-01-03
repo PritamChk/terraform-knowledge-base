117dae7719e1f609f93c401ebf434af51a772fec
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2026-01-03 11:24:05 +0600

    init: quiz app  terraform provider setup

---
+++ aws/App/TwoTireApp/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "tf-learning-s3b-pc"
+    key    = "quizapptf/stg/terraform.tfstate"
+    region = "ap-south-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.27.0"
+    }
+  }
+}
