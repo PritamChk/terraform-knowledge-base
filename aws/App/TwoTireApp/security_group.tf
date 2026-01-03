2218633b3974a5604ca93084b7eb36f8d89affe3
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2026-01-04 02:50:07 +0600

    sg change

--- aws/App/TwoTireApp/security_group.tf
+++ aws/App/TwoTireApp/security_group.tf
@@ -4,39 +4,84 @@ data "aws_prefix_list" "s3" {
 }
 
 module "private_ec2_sg" {
-  source = "terraform-aws-modules/security-group/aws"
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
 
   name        = "${var.quiz_vpc_name}-private-sg"
-  description = "Security group for App VM to communicate with LB using HTTP port open within VPC"
+  description = "Allow Access ONLY from Public EC2"
   vpc_id      = module.app_vpc.vpc_id
 
-  ingress_with_cidr_blocks = [
+  # --- INGRESS (Keep as is) ---
+  computed_ingress_with_source_security_group_id = [
     {
-      from_port   = 8000
-      to_port     = 9000
-      protocol    = "tcp"
-      description = "quiz-app-service ports"
-      cidr_blocks = join(",", module.app_vpc.public_subnets_cidr_blocks)
+      rule                     = "ssh-tcp"
+      source_security_group_id = module.public_ec2_sg.security_group_id
     },
     {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      description = "quiz-app-service ports"
-      cidr_blocks = join(",", module.app_vpc.public_subnets_cidr_blocks)
+      rule                     = "http-8080-tcp" # Ensure this matches your app port (e.g. 8000)
+      source_security_group_id = module.public_ec2_sg.security_group_id
     }
   ]
-  ## For S3
+  number_of_computed_ingress_with_source_security_group_id = 2
+
+  # --- EGRESS (The Fix) ---
+
+  # 1. Keep S3 Access (You already have this)
   egress_with_prefix_list_ids = [
     {
       from_port       = 443
       to_port         = 443
       protocol        = "tcp"
       prefix_list_ids = data.aws_prefix_list.s3.id
-      description     = "Allow secure access to S3 Gateway Endpoint"
+    }
+  ]
+
+  # 2. ADD THIS: Allow Internet Access (For Git, Pip, Docker, etc.)
+  # Traffic goes -> Private SG -> NAT Gateway -> Internet
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      description = "Allow all outbound traffic"
+      cidr_blocks = "0.0.0.0/0"
     }
   ]
 }
+# module "private_ec2_sg" {
+#   source = "terraform-aws-modules/security-group/aws"
+
+#   name        = "${var.quiz_vpc_name}-private-sg"
+#   description = "Security group for App VM to communicate with LB using HTTP port open within VPC"
+#   vpc_id      = module.app_vpc.vpc_id
+
+#   ingress_with_cidr_blocks = [
+#     {
+#       from_port   = 8000
+#       to_port     = 9000
+#       protocol    = "tcp"
+#       description = "quiz-app-service ports"
+#       cidr_blocks = join(",", module.app_vpc.public_subnets_cidr_blocks)
+#     },
+#     {
+#       from_port   = 22
+#       to_port     = 22
+#       protocol    = "tcp"
+#       description = "quiz-app-service ports"
+#       cidr_blocks = join(",", module.app_vpc.public_subnets_cidr_blocks)
+#     }
+#   ]
+#   ## For S3
+#   egress_with_prefix_list_ids = [
+#     {
+#       from_port       = 443
+#       to_port         = 443
+#       protocol        = "tcp"
+#       prefix_list_ids = data.aws_prefix_list.s3.id
+#       description     = "Allow secure access to S3 Gateway Endpoint"
+#     }
+#   ]
+# }
 
 # =========================================================
 # 1. PUBLIC SG (For the Jump Server / Public EC2)
