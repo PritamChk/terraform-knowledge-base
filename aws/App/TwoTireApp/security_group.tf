9d291e704b32949311d2f429d27cbadd80243b77
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2026-01-04 00:11:27 +0600

    Fix : load balancer ingress from internet allow all

--- aws/App/TwoTireApp/security_group.tf
+++ aws/App/TwoTireApp/security_group.tf
@@ -22,9 +22,9 @@ module "ec2_sg_quiz_vpc" {
 module "load_balancer" {
   source = "terraform-aws-modules/security-group/aws"
 
-  name        = "${var.quiz_vpc_name}-lb-sg"
-  description = "Security group for LB to communicate with HTTP port open within VPC"
-  vpc_id      = module.app_vpc.vpc_id
-
-  ingress_rules = ["http-80-tcp"]
+  name                = "${var.quiz_vpc_name}-lb-sg"
+  description         = "Security group for LB to communicate with HTTP port open within VPC"
+  vpc_id              = module.app_vpc.vpc_id
+  ingress_cidr_blocks = "0.0.0.0/0"
+  ingress_rules       = ["http-80-tcp"]
 }
